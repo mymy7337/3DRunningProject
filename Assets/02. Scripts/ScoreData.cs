defe403f22070e6ce9e9408ee796a815be3d512c
jo-ji-hyun <newterr@naver.com>
2025-08-14 15:09:54 +0900

    [Docs] 데이터 저장할 준비
    제이슨 같은걸로

---
+++ Assets/02. Scripts/ScoreData.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable] // === 저장할 정보 정의 ===
+public class ScoreData 
+{
+    public int highScore;
+    public int currentScore;
+}
