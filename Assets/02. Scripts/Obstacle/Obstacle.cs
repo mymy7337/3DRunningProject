1f3d8190c6dc491c5b275041db1ab7a0d6249128
박상현 <night8641@naver.com>
2025-08-14 20:11:27 +0900

    [Fix]버스의 콜라이더 수정 및 추가

---
+++ Assets/02. Scripts/Obstacle/Obstacle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public LayerMask playerLayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // other의 레이어가 playerLayer에 포함되는지 확인
+        if ((playerLayer.value & (1 << other.gameObject.layer)) != 0)
+        {
+            Debug.Log("플레이어 감지됨! (레이어 기반)"); //플레이어죽이기
+        }
+    }
+}
