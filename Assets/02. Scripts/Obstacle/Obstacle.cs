8bbc477677e71725944ed7cf9c00308b405cc7b9
박상현 <night8641@naver.com>
2025-08-18 06:45:21 +0900

    [Feat] 게임 오버시 정지

--- Assets/02. Scripts/Obstacle/Obstacle.cs
+++ Assets/02. Scripts/Obstacle/Obstacle.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("플레이어 감지됨!"); //플레이어죽이기(게임오버)
+            Time.timeScale = 0f;
+            //게임오버 UI 띄우기
         }
     }
 }
