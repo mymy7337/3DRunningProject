c855f981d8fb89a0f0ffb6550638f31ef217e735
mymy7337 <minyeong7337@gmail.com>
2025-08-21 09:38:21 +0900

    [Revert] PlayerController,PlayerCustomizer 롤백
    내용이 삭제되어서 이전 커밋 복구

--- Assets/02. Scripts/Player/PlayerAnimationController.cs
+++ Assets/02. Scripts/Player/PlayerAnimationController.cs
@@ -9,7 +9,7 @@ public class PlayerAnimationController : MonoBehaviour
     public static readonly int isJumping = Animator.StringToHash("IsJump");
     public static readonly int isCrouch = Animator.StringToHash("IsCrouch");
 
-    private Animator animator;
+    public Animator animator;
 
     private bool isRun;
 
