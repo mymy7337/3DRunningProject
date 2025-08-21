3c367da7f447b49bce3c786df303b132dca3cb35
Insung Park <sleepwithoutadream@gmail.com>
2025-08-21 15:02:35 +0900

    testing

---
+++ Assets/02. Scripts/ColorPicker/Materials 2.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Materials : MonoBehaviour
+{
+    public SkinnedMeshRenderer[] renderers;
+    //1. 피부 2. 헤어 3. 렌즈 4~장비, etc
+}
